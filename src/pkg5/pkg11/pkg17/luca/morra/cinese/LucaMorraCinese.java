2ab0a08790a73a20a0868f29d8191ee06fab6974
tss <tss@debian-tss24>
2017-12-05 11:06:52 +0100

    morra cinese

---
+++ src/pkg5/pkg11/pkg17/luca/morra/cinese/LucaMorraCinese.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg5.pkg11.pkg17.luca.morra.cinese;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author tss
+ */
+public class LucaMorraCinese {
+
+    static String pctiro = "";
+    static String tiro = "";
+    static String ris = "";
+    static int punti = 0;
+    static int puntipc = 0;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // in quanti gicoatori volete giocare
+
+        String nome = JOptionPane.showInputDialog("Qual'è il nome del giocatore?");
+        // quanti tiri vuoi fare?
+        String ntiri = JOptionPane.showInputDialog("A quanti punti vuoi arrivare? ");
+        double tiri = Double.parseDouble(ntiri);
+        // gioca
+        JOptionPane.showMessageDialog(null, "La partita ha inizio!");
+        do {
+            tiro = JOptionPane.showInputDialog("Fai il tuo tiro: scegli tra\n" + lista() + "\n\n-----------------------------------\nPunteggi attuali:\n" + nome + " : " + punti + "\nPC : " + puntipc).toUpperCase();
+            if (tiro.equals("F") || tiro.equals("C") || tiro.equals("S")) {
+                acaso();
+                morra();
+                JOptionPane.showMessageDialog(null, ris);
+
+            } else {
+                JOptionPane.showMessageDialog(null, "we ciccio inserisci una mossa corretta");
+            }
+
+        } while (!(punti == tiri || puntipc == tiri));
+        if (punti > puntipc) {
+            JOptionPane.showMessageDialog(null, "Congratulazioni! hai vinto !\nIl punteggio finale era: \nPC : " + puntipc + "\n" + nome + " : " + punti);
+        } else {
+            JOptionPane.showMessageDialog(null, "Hai perso!\nIl punteggio finale era: \nPC : " + puntipc + "\n" + nome + " : " + punti);
+        }
+
+        // dai un punteggio
+        // se pari vuoi fare la bella?
+        // ha vinto azzdragon
+        // vuoi fare un altra partita?
+    }
+
+    static String acaso() {
+        String pctiro = "";
+
+        double tiropc = Math.random();
+        if (tiropc <= 0.33) {
+            pctiro = "F";
+        }
+        if (tiropc >= 0.66) {
+            pctiro = "C";
+        }
+        if (tiropc < 0.33 & tiropc > 0.66) {
+            pctiro = "S";
+        }
+        return pctiro;
+    }
+
+    static String lista() {
+        String elenco;
+        elenco = "F = forbici ;\nS = sasso ;\nC = carta.";
+        return elenco;
+    }
+
+    static String morra() {
+
+        //confrontare se sono uguali
+        if (tiro.equals(pctiro)) {
+            ris = "Pari!";
+        } else {
+            switch (tiro) {
+
+                case "F":
+                    if (pctiro == "C") {
+                        ris = "hai vinto te!";
+                        punti += 1;
+                    } else {
+                        ris = "ha vinto il pc!";
+                        puntipc += 1;
+                    }
+                    break;
+                case "C":
+                    if (pctiro == "F") {
+                        ris = "ha vinto il pc!";
+                        puntipc += 1;
+                    } else {
+                        ris = "hai vinto te!";
+                        punti += 1;
+                    }
+                    break;
+                case "S":
+                    if (pctiro == "F") {
+                        ris = "hai vinto te!";
+                        punti += 1;
+                    } else {
+                        ris = "ha vinto il pc!";
+                        puntipc += 1;
+                    }
+                    break;
+
+            }
+        }
+        return ris;
+
+    }
+}
